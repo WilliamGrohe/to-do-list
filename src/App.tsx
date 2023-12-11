85f54b2882a974583288b7d667ab1687c23246b9
WilliamGrohe <william.grohe@gmail.com>
2023-12-10 23:08:02 -0300

    Created card task

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import LogoImg from "./assets/Logo.svg";
 import ClipboardIcon from "./assets/Clipboard.svg";
-import TrashIcon from './assets/trash.svg'
+import TrashIcon from "./assets/trash.svg";
 
 import { InputTask } from "./components/InputTask";
-
 
 export function App() {
   return (
@@ -35,6 +34,55 @@ export function App() {
             <p>Crie tarefas e organize seus itens a fazer</p>
           </div>
 
+          <div className="bg-[var(--gray-500)] text-[var(--gray-100)] text-sm flex gap-3 p-4 items-center rounded-lg transition-all">
+            <div className="">
+              <input
+                type="checkbox"
+                id="switch2"
+                className="h-0 w-0 hidden peer"
+              />
+
+              <label
+                htmlFor="switch2"
+                className="cursor-pointer w-3 h-3 ring ring-[var(--blue)] hover:ring-[var(--dark-blue)] rounded-full block relative 
+                          after:absolute after:top-[-4px] after:text-zinc-50
+                          peer-checked:after:content-['\2714']
+                          peer-checked:ring-[var(--dark-purple)] peer-checked:bg-[var(--dark-purple)]
+                          peer-checked:hover:ring-[var(--purple)] peer-checked:hover:bg-[var(--purple)]
+                          transition-all
+                        "
+              ></label>
+            </div>
+            <div className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+              iste, error porro suscipit officiis delectus. Tempora hic, eius ad
+              totam maiores voluptas deleniti ducimus ratione placeat illum
+              nemo? Molestias, aliquam?
+            </div>
+            <div className=" ">
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="stroke-[var(--gray-300)] hover:stroke-[var(--danger)] p-1 rounded-md hover:bg-[var(--gray-400)] transition-all"
+                  width="36"
+                  height="36"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="#058"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M4 7l16 0" />
+                  <path d="M10 11l0 6" />
+                  <path d="M14 11l0 6" />
+                  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                </svg>
+              </button>
+            </div>
+          </div>
         </div>
       </main>
     </>
